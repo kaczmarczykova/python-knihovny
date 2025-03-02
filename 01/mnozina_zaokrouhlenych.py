8d209ada7f09eb1a90fe32365e9c9cc8b8331fb6
Zuzana Kaczmarczyková <kaczmarczykova@gmail.com>
2025-03-02 09:39:48 +0100

    Ukol lekce 1

--- 01/mnozina_zaokrouhlenych.py
+++ 01/mnozina_zaokrouhlenych.py
@@ -1,4 +1,5 @@
 # Napiš funkci, která vrátí ze iterovatelného vstupu množinu zaokrouhlených hodnot
 
 def mnozina_zaokrouhlenych(vstup):
-    ...
+    mnozina = {round(cislo) for cislo in vstup}
+    return mnozina
