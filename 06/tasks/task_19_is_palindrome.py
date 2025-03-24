e310f8738d168a7bd2dae2a9104cee95ac7009ae
Zuzana Kaczmarczyková <kaczmarczykova@gmail.com>
2025-03-24 20:15:24 +0100

    19 is palindrome?

--- 06/tasks/task_19_is_palindrome.py
+++ 06/tasks/task_19_is_palindrome.py
@@ -4,4 +4,8 @@ def is_palindrome(s: str) -> bool:
     """
     Vrátí True, pokud je řetězec palindrom.
     """
-
+    length = len(s)
+    for i in range(length // 2):
+        if s[i] != s[length - i - 1]:
+            return False
+    return True
