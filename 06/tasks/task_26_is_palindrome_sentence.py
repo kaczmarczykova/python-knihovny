7f0ebc91cbb17c6d9d032f6cec4dbc36e88fd026
Dragca <49131083+Dragca@users.noreply.github.com>
2025-05-06 22:15:59 +0200

    26 return True if sentence is palindrom (#59)

--- 06/tasks/task_26_is_palindrome_sentence.py
+++ 06/tasks/task_26_is_palindrome_sentence.py
@@ -4,4 +4,7 @@ def is_palindrome_sentence(s: str) -> bool:
     """
     Vrátí True, pokud je věta palindrom (ignoruje mezery, interpunkci a velikost písmen).
     """
+# creates a list consisting only of lowercase letters of the original string
+    s_list = [char.lower() for char in s if char.isalpha()]
 
+    return s_list == s_list[::-1]
